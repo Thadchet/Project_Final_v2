ae09ea8c552434a940c66aba1df8fabb7a2869c7
thadchet <sogood_boss@hotmail.com>
2018-12-05 23:05:59 +0700

    eiei

--- src/sharedObject/RenderableHolder.java
+++ src/sharedObject/RenderableHolder.java
@@ -17,13 +17,13 @@ public class RenderableHolder {
 	public static AudioClip wordDead;
 	public static AudioClip gameover;
 	public static AudioClip gameplay;
-	public static AudioClip gameplay2 ;
+	public static AudioClip gameplay2;
 	public static AudioClip heal;
 	public static AudioClip skill1;
 	public static AudioClip skill2;
 	public static Image balloon;
-	public static Image explosion ;
-	public static Image spell ;
+	public static Image explosion;
+	public static Image spell;
 	public static String image_path = ClassLoader.getSystemResource("image/").toString();
 	public static String sound_path = ClassLoader.getSystemResource("sound/").toString();
 	static {
@@ -41,13 +41,13 @@ public class RenderableHolder {
 		wordDead = new AudioClip(sound_path + "wordDead.mp3");
 		gameover = new AudioClip(sound_path + "gameover.mp3");
 		gameplay = new AudioClip(sound_path + "game.mp3");
-		gameplay2 = new AudioClip(sound_path+"game2.m4a");
+		gameplay2 = new AudioClip(sound_path + "game2.m4a");
 		balloon = new Image(image_path + "balloon.gif");
 		heal = new AudioClip(sound_path + "heal.mp3");
 		skill1 = new AudioClip(sound_path + "skill1.m4a");
 		skill2 = new AudioClip(sound_path + "skill2.mp3");
-		explosion = new Image(image_path+"explosion.gif");
-		spell = new Image(image_path+"spell.gif");
+		explosion = new Image(image_path + "explosion.gif");
+		spell = new Image(image_path + "spell.gif");
 	}
 
 	public void add(IRenderable entity) {
@@ -67,27 +67,56 @@ public class RenderableHolder {
 		for (IRenderable w : entities) {
 			if (w instanceof Wizard) {
 				for (IRenderable i : entities) {
-					if (i instanceof Word) {
+					if (i instanceof Word && !(i.getClass().getSimpleName().equals("WordHeal"))) {
 						if (((Word) i).getWordstring().equalsIgnoreCase(temp)) {
 							((Wizard) w).addScore();
 							((Word) i).setIsvisible(false);
+							Thread t = new Thread(new Runnable() {
+
+								@Override
+								public void run() {
+									// TODO Auto-generated method stub
+									try {
+										Thread.sleep(250);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									((Word) i).setIsdestory(true);
+								}
+							});
+							t.start();
 							wordDead.play();
-							return (Word) i ;
+							return (Word) i;
 						}
 					}
 					if (i instanceof WordHeal) {
 						if (((WordHeal) i).getWordstring().equalsIgnoreCase(temp)) {
 							((Wizard) w).addScore();
 							((Word) i).setIsvisible(false);
+							Thread t = new Thread(new Runnable() {
+								@Override
+								public void run() {
+									// TODO Auto-generated method stub
+									try {
+										Thread.sleep(250);
+									} catch (InterruptedException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									((Word) i).setIsdestory(true);
+								}
+							});
+							t.start();
 							heal.play();
-							return (Word) i ;		
+							return (Word) i;
 						}
-					}			
+					}
 				}
 			}
 		}
 		return null;
-		
+
 	}
 
 	public void draw(GraphicsContext gc) {
